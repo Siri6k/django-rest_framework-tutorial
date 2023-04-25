4dc2fa0ec9274f0e657816379e87ef14adb39a10
Sirisk <adamchrisk@gmail.com>
2023-04-25 12:35:30 +0200

    add login to the browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(snippets.urls)),
 ]
+# for login
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
